8a1c4d60b093e52ff01d536b1e4ad52bba8e6262
AreYouLucky <johnmichaelcagadas@gmail.com>
2026-07-02 10:21:43 +0800

    small route fixes

--- routes/backend/committee.php
+++ routes/backend/committee.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth', 'committee'])->group(function () {
     Route::get('/committee-review-page', [CommitteeReviewController::class, 'committeeReviewPage']);
     Route::get('/committee-review-batches', [CommitteeReviewController::class, 'CommitteeReviewBatches'])->name('view-committee-review-batches');
-    Route::get('/view-committee-review-batch/{id}', [CommitteeReviewController::class, 'viewApprovalRequests'])->name('view-committee-review-batches');
+    Route::get('/view-committee-review-batch/{id}', [CommitteeReviewController::class, 'viewApprovalRequests'])->name('view-committee-review-batches-by-id');
     Route::get('/committee-review-request/{holdingsID}', [CommitteeReviewController::class, 'ReviewRequest']);
     Route::post('/submit-committee-review', [CommitteeReviewController::class, 'submitReview']);
     Route::get('/generate-committee-report', [CommitteeReviewController::class, 'generateCommitteeReport']);
