b75c4a0657264f358937fd665966db3604d0e1d7
AreYouLucky <johnmichaelcagadas@gmail.com>
2026-07-02 10:19:34 +0800

    update route name from home login

--- routes/authentication/authentication.php
+++ routes/authentication/authentication.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AuthController;
 Route::middleware('guest')->group(function () {
     Route::get('/', function () {
         return Inertia::render('auth/auth-page');
-    })->name('login');
+    });
     Route::post('/request-login', [AuthController::class, 'login']);
 });
 Route::middleware('auth')->group(function () {
